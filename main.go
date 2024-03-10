c2e3a43f55b45e7d4e80ec6f2b94129f5c016d03
Webdev <arthursantosdeveloper@gmail.com>
2024-03-10 17:21:24 -0300

    pocketbase initial setup

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo/v5"
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/apis"
+	"github.com/pocketbase/pocketbase/core"
+)
+
+// main is the entry point of the application
+func main() {
+	app := pocketbase.New()
+	fmt.Println("Hello, World!", io.Writer(os.Stdout))
+	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+		// add new "GET /hello" route to the app router (echo)
+		log.Println(e.App)
+		e.Router.AddRoute(echo.Route{
+			Method: http.MethodGet,
+			Path:   "/hello",
+			Handler: func(c echo.Context) error {
+				return c.String(200, "Hello world!")
+			},
+			Middlewares: []echo.MiddlewareFunc{
+				apis.ActivityLogger(app),
+			},
+		})
+
+		return nil
+	})
+
+	if err := app.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
